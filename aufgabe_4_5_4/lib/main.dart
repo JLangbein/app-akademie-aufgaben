7a4b8cafb44ab94ed015642f8ee99bdb7231fcd9
Johannes Langbein <jllangbein@gmail.com>
2025-07-21 10:59:35 +0200

    Aufgabe 2

--- aufgabe_4_5_4/lib/main.dart
+++ aufgabe_4_5_4/lib/main.dart
@@ -12,17 +12,33 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('MyApp'),
-          centerTitle: true,
-          backgroundColor: Colors.lightGreen,
-        ),
-        // body: NewsPage(),
-        // body: LikesPage(),
-        body: ProfilePage()
+    return MaterialApp(home: AppHome());
+  }
+}
+
+class AppHome extends StatefulWidget {
+  const AppHome({super.key});
+
+  @override
+  State<AppHome> createState() => _AppHomeState();
+}
+
+class _AppHomeState extends State<AppHome> {
+  // states (attributes)
+  int _currentIndex = 0;
+  static const List<Widget> screens = [NewsPage(), LikesPage(), ProfilePage()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MyApp'),
+        centerTitle: true,
+        backgroundColor: Colors.lightGreen,
       ),
+      // body: NewsPage(),
+      // body: LikesPage(),
+      body: ProfilePage(),
     );
   }
 }
